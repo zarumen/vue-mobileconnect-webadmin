7b6aefdc29d07a6314d40a69ab5cb66c7fe1ccaa
zarumen <prinzbiz@gmail.com>
2018-06-19 09:55:19 +0700

    add form add campaign components

--- src/components/form-add-campaign-parser.vue
+++ src/components/form-add-campaign-parser.vue
@@ -16,53 +16,56 @@ export default {
 </script>
 
 <template>
-  <v-flex 
-    xs6
-    sm5 
+  <v-dialog
+    v-model="addParserDialog"
+    persistent
+    width="600px"
   >
-    <v-card>
-      <v-subheader>Add Context Parser</v-subheader>
-      <v-card-text>
-        <v-text-field
-          solo-inverted 
-          label="Remove Character"
-        />
-        <v-text-field 
-          prepend-icon="message"
-          label="Invalid Format Message"
-        />
-        <v-select 
-          v-model="validateType"
-          :items="validateTypeList"
-          :hint="`${validateType}`"
-          item-text="value"
-          item-value="condition"
-          label="Condition Type"
-          prepend-icon="assignment"
-          persistent-hint
-        />
-        <v-text-field 
-          :disabled="validateType"
-          solo-inverted 
-          label="Condition (Regular Expression)"
-        />
-      </v-card-text> 
-      <v-card-actions>
-        <v-spacer/>
-        <v-btn 
-          color="blue" 
-          class="white--text"
-        >
-          Save
-        </v-btn>
-        <v-btn 
-          flat
-        >
-          Cancel
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-flex>
+    <v-form>
+      <v-card>
+        <v-subheader>Add Context Parser</v-subheader>
+        <v-card-text>
+          <v-text-field
+            solo-inverted 
+            label="Remove Character"
+          />
+          <v-text-field 
+            prepend-icon="message"
+            label="Invalid Format Message"
+          />
+          <v-select 
+            v-model="validateType"
+            :items="validateTypeList"
+            :hint="`${validateType}`"
+            item-text="value"
+            item-value="condition"
+            label="Condition Type"
+            prepend-icon="assignment"
+            persistent-hint
+          />
+          <v-text-field 
+            :disabled="validateType"
+            solo-inverted 
+            label="Condition (Regular Expression)"
+          />
+        </v-card-text> 
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn 
+            color="blue" 
+            class="white--text"
+          >
+            Save
+          </v-btn>
+          <v-btn 
+            flat
+          >
+            Cancel
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-form>
+  </v-dialog>
 </template>
 
 <style lang="scss" module>
