a85fa3f8c1ace5d89d53e06d4dd15ea60a51916d
Jirayudech <jirayudech@gmail.com>
2019-04-09 14:55:05 +0700

    แก้ embeded code เพิ่ม /

--- src/router/views/campaignwidget.vue
+++ src/router/views/campaignwidget.vue
@@ -125,7 +125,7 @@ export default {
       handler(val) {
         this.code =  '<iframe width="560" height="315" src="https://sms2mkt.com/campaignwidgetview/'+this.$route.params.campaignId+'/'+this.campaignWidget.offset+'/'+this.campaignWidget.caption+'/'+val.units+'/'+this.campaignWidget.multiplier+'/'+this.campaignWidget.fontColor+'" frameborder="0" ></iframe>'
         this.code2 =  '<iframe width="560" height="315" src="https://sms2mkt.com/campaignwidgetview2/'+this.$route.params.campaignId+'/'+this.campaignWidget.offset+'/'+this.campaignWidget.caption+'/'+val.units+'/'+this.campaignWidget.multiplier+'/'+this.campaignWidget.fontColor+'" frameborder="0" ></iframe>'
-        this.code3 =  '<iframe width="560" height="315" src="https://sms2mkt.com/campaignwidgetview3/'+this.$route.params.campaignId+'/'+this.campaignWidget.offset+'/'+this.campaignWidget.caption+'/'+val.units+'/'+this.campaignWidget.multiplier+'/'+this.campaignWidget.fontColor+'production" frameborder="0" ></iframe>'
+        this.code3 =  '<iframe width="560" height="315" src="https://sms2mkt.com/campaignwidgetview3/'+this.$route.params.campaignId+'/'+this.campaignWidget.offset+'/'+this.campaignWidget.caption+'/'+val.units+'/'+this.campaignWidget.multiplier+'/'+this.campaignWidget.fontColor+'/production" frameborder="0" ></iframe>'
         
         firestoreApp.collection("campaignWidget").doc(this.$route.params.campaignId).set(this.campaignWidget).then(function() {
           console.log("Campaign Widget Successfully Written!");
