9839bb46bded27233659808996d0a0a65649583a
zarumen <prinzbiz@gmail.com>
2019-07-09 22:16:33 +0700

    change config hosting (not cache)

--- src/router/views/campaignwidgetview3.vue
+++ src/router/views/campaignwidgetview3.vue
@@ -293,7 +293,7 @@ export default {
         <v-layout> 
           <v-flex 
             xs-12
-            style="width: 100%; padding-left: 20px; padding-bottom: 20px; padding-right: 20px;"        
+            style="width: 100%; padding-right: 20px; padding-bottom: 20px; padding-left: 20px;"        
           >
             <div class="text-xs-center">
               <canvas id="widget-chart" />
